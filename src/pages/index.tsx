a6de31e5e452ef978718eb075daf548e728ce0ae
Alquipo <alquiponeto@outlook.com.br>
2021-02-05 10:38:14 -0300

    fix: styled components in NextJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Title } from "../styles/pages/Home";
+
 export default function Home() {
   return (
     <div>
-      <h1>hello World</h1>
+      <Title>hello World</Title>
     </div>
   );
 }
